963e5068783a28b1d8f6c7b0ce2778bf3fd92d14
Gauravv09 <115243114+Gauravv09@users.noreply.github.com>
2023-12-04 13:14:40 +0530

    Add files via upload

---
+++ fsd04/process_form.php
@@ -0,0 +1,35 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fsd04"; 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch form data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+
+
+    $sql = "INSERT INTO users (username, email, phone, password) VALUES ('$username', '$email', '$phone', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
